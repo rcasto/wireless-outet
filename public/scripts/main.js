beaea111230eb2521355c02d58e4bb238e645323
Richie Casto <Richie Casto>
2017-03-04 20:23:11 -0800

    refactor a bit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,19 @@
 (function () {
-    function buildOutletHTML(outlets) {
-        var outletContainer = document.createElement('div'), outletHTML;
+    function buildOutletSwitches(outlets) {
+        var outletSwitchContainer = document.createElement('div'), outletSwitch;
         outlets.forEach(function (outlet, i) {
-            outletHTML = createOutletSwitch(outlet);
-            outletHTML.querySelector('.slider').onclick = function () {
-                Request.getJSON('/api/toggle/' + i, function (json) {
-                    console.log(json);
+            outletSwitch = createOutletSwitch(outlet);
+            outletSwitch.querySelector('input').onclick = function (event) {
+                event.preventDefault();
+            };
+            outletSwitch.querySelector('.slider').onclick = function () {
+                Request.getJSON('/api/toggle/' + i, function (newOutlet) {
+                    setOutletSwitchState(newOutlet, outletSwitch);
                 });
             };
-            outletContainer.appendChild(outletHTML);
+            outletSwitchContainer.appendChild(outletSwitch);
         });
-        return outletContainer;
+        return outletSwitchContainer;
     }
 
     function createOutletSwitch(outlet) {
@@ -19,21 +22,31 @@
         var input = document.createElement('input');
         var div = document.createElement('div');
         label.className = 'switch';
-        span.innerHTML = outlet.name;
         input.type = 'checkbox';
-        input.checked = outlet.isOn;
         div.className = 'slider round';
         label.appendChild(span)
         label.appendChild(input)
         label.appendChild(div);
+        if (outlet) {
+            setOutletSwitchState(outlet, label);
+        }
         return label;
+    }
+
+    function setOutletSwitchState(outlet, outletSwitch) {
+        outletSwitch.querySelector('input').checked = outlet.isOn;
+        outletSwitch.querySelector('span').innerHTML = generateOutletStateString(outlet);
+    }
+
+    function generateOutletStateString(outlet) {
+        return outlet.name + ' (' + (outlet.isOn ? 'On' : 'Off')  + ')';  
     }
 
     // Initialize state of switch toggles in client view based off state sent from
     // server when starting up
     window.onload = function () {
         Request.getJSON('/api/state/', function (outlets) {
-            var outletHTML = buildOutletHTML(outlets);
+            var outletHTML = buildOutletSwitches(outlets);
             document.body.appendChild(outletHTML);
         });
     };
