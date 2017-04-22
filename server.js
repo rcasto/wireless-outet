bb19b00dc6e610e1e2fc938cf39677b31b2252d1
Richie Casto <Richie Casto>
2017-04-22 14:56:09 -0700

    add activity hook

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var outletAPI = require('./lib/outlet-api');
+var activityHook = require('./lib/activityHook');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -12,4 +13,5 @@ app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+activityHook.init();
 app.listen(port, () => console.log(`Server listening on port ${port}`));
